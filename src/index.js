b8070c84c47fa6c2d371e1691c9b94ff49c5c7be
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-04-05 14:32:39 -0400

    Changed Api, Changed HTML to accomidate new API, updated js file.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-    getData()
-    const form = document.getElementById('search-word-form');
-    const input = document.getElementById('new-word-input').value;
-    const newWords = document.getElementById('words')
-    form.addEventListener('submit', displayWords);
-    
+    getQuotes()
 })
-const BASE_URL = 'https://wordsapiv1.p.mashape.com/words/'
-function getData() {
-    const input = document.getElementById('new-word-input').value;
-    fetch(`BASE_URL + ${input}`, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "b6af3e1462msh77dd79715f95790p129d91jsn33c69e8926af",
-		"x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
-	}
-})
-    .then(response => response.json())
-    .then(response => {
-        console.log(response);
-        resonse
+
+let quotes = document.querySelector('.quotes')
+let button = document.querySelector('button')
+button.addEventListener('click', getQuotes)
+
+function getQuotes() {
+    fetch('http://quotes.stormconsultancy.co.uk/random.json')
+    .then(res => res.json())
+    .then(data => {
+        console.log('Sucess:', data)
+        quotes.innerHTML = 
+        `
+        <h2><em>${data.quote}</em></h2>
+        <h4>${data.author}</h4>
+        `
     })
-    .catch(err => {
-        console.error(err);
-    });
+    .catch((error) => {
+        console.error(error);
+      });
 }
