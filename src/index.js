d65ed80da241b30df34449ec8134ade489353b41
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-04-06 02:25:56 -0400

    Added like btn, worked on button css and edited HTML.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ let quotes = document.querySelector('#quotes')
 let authors = document.querySelector('#authors')
 
 
-
-
-
 function getQuotes() {
     fetch('http://quotes.stormconsultancy.co.uk/random.json')
     .then(res => res.json())
     .then(data => {
         console.log('Sucess:', data)
         newQuote = `<h2>'<em>${data.quote}</em>'</h2>`;
-        newAuthor = `<h4>${data.author}</h4>`;
+        newAuthor = `<h4><em>~ </em>${data.author}</h4>`;
         quotes.innerHTML = newQuote;
         authors.innerHTML = newAuthor; 
        
@@ -20,12 +17,18 @@ function getQuotes() {
         console.error(error);
       });
 }
+
+    let like = document.querySelector('#like')
+    like.addEventListener('click', likeHeart)
+
+
+function likeHeart() {
+   return like.src="http://forsage.thejarviscorp.com/wp-content/uploads/2021/04/like-grey.png";
+}
+
+
 let button = document.querySelector('button')
 button.addEventListener('click', getQuotes)
 document.addEventListener('DOMContentLoaded', () => {
     getQuotes()
 })
-
-function makeUpperCase() {
-
-}
