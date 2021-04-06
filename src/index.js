f56061b0665a4db67d1e584c0003cefe1c2632d7
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-04-05 23:48:00 -0400

    Added Bootstrap, Updated Index.html and index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
-    getQuotes()
-})
+let quotes = document.querySelector('#quotes')
+let authors = document.querySelector('#authors')
 
-let quotes = document.querySelector('.quotes')
-let button = document.querySelector('button')
-button.addEventListener('click', getQuotes)
+
+
+
 
 function getQuotes() {
     fetch('http://quotes.stormconsultancy.co.uk/random.json')
     .then(res => res.json())
     .then(data => {
         console.log('Sucess:', data)
-        quotes.innerHTML = 
-        `
-        <h2><em>${data.quote}</em></h2>
-        <h4>${data.author}</h4>
-        `
+        newQuote = `<h2><em>${data.quote}</em></h2>`;
+        newAuthor = `<h4>${data.author}</h4>`;
+        quotes.innerHTML = newQuote;
+        authors.innerHTML = newAuthor; 
+       
     })
     .catch((error) => {
         console.error(error);
       });
+}
+let button = document.querySelector('button')
+button.addEventListener('click', getQuotes)
+document.addEventListener('DOMContentLoaded', () => {
+    getQuotes()
+})
+
+function makeUpperCase() {
+
 }
