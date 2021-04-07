bfc05567e3166ecd68829710527eb763204e74ad
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-04-07 09:06:54 -0400

    updated js and html

--- src/index.js
+++ src/index.js
@@ -1,34 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    getQuote()
+    let button = document.querySelector('button')
+    button.addEventListener('click', getQuote)
+})
+
 let quotes = document.querySelector('#quotes')
 let authors = document.querySelector('#authors')
 
-
-function getQuotes() {
+function getQuote() {
     fetch('http://quotes.stormconsultancy.co.uk/random.json')
     .then(res => res.json())
     .then(data => {
         console.log('Sucess:', data)
-        newQuote = `<h2>'<em>${data.quote}</em>'</h2>`;
-        newAuthor = `<h4><em>~ </em>${data.author}</h4>`;
+        let newQuote = `<h2>'<em>${data.quote}</em>'</h2>`;
+        let newAuthor = `<h4><em>~ </em>${data.author}</h4>`;
         quotes.innerHTML = newQuote;
         authors.innerHTML = newAuthor; 
-       
     })
     .catch((error) => {
         console.error(error);
       });
 }
 
-    let like = document.querySelector('#like')
-    like.addEventListener('click', likeHeart)
-
-
-function likeHeart() {
-   return like.src="http://forsage.thejarviscorp.com/wp-content/uploads/2021/04/like-grey.png";
-}
-
-
-let button = document.querySelector('button')
-button.addEventListener('click', getQuotes)
-document.addEventListener('DOMContentLoaded', () => {
-    getQuotes()
-})
+// when user clicks a like button data is saved in their favorites
