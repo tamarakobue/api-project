152e9a67583e7cec8fe2ac03a77000ad0cbe6589
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-04-08 11:25:03 -0400

    remove asyc and finally got likebutton working

--- src/index.js
+++ src/index.js
@@ -1,69 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
     // functions to call upon content load
-    getQuote(); 
+    getQuote()
 
-    // global variables 
+    // global variables
     let button = document.querySelector('button');
+    let likeBtn = document.querySelector('#fav-button');
+
+ 
+
+    // event listeners
+    button.addEventListener('click', getQuote);
+    likeBtn.addEventListener('click', likeQuote);
+})
+
+  // use captured response and display quotes & authors
+  function getQuote() {
     let quotes = document.querySelector('#quotes');
     let authors = document.querySelector('#authors');
-    let likeBtn = document.querySelector('#liked');
-    let favQuotesContainer = document.querySelector(".fav-quotes-container")
-    let currentQuote = [];
-
-    // variable event listeners
-    button.addEventListener('click', getQuote);
-    likeBtn.addEventListener('click', favQuotes);
+    let likeBtn = document.querySelector('#fav-button');
     
+    //let currentQuote = [];
 
-    // use captured response and display quotes & authors
-    function getQuote() {
-        fetchQuote().then((data) => {
-            currentQuote.push(data); // push data into new object for like-btn accessibility 
-            quotes.innerHTML = `<h2>'<em>${data.quote}</em>'</h2>`;
-            authors.innerHTML = `<h4><em> ~  </em>${data.author}</h4>`;
-        })
-    }
-    console.log('CURRENT QUOTE CONTENTS', currentQuote);
+    fetch('http://quotes.stormconsultancy.co.uk/random.json')
+    .then(res => res.json())
+    .then(data => {
 
-    // fetch random single quote 
-    async function fetchQuote() {
-        let response = await fetch('http://quotes.stormconsultancy.co.uk/random.json');
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json(); 
-        // use .json() at end of response to tell it what kind of format? (i.e. .text for promise resolved in raw text. )
-        return data;
-    }
+        // display random quote
+        console.log('Success:', data)
+        let newQuote = `<h2>'<em>${data.quote}</em>'</h2>`;
+        let newAuthor = `<h4>'<em>${data.author}</em>'</h4>`;
+        quotes.innerHTML = newQuote;
+        authors.innerHTML = newAuthor; 
 
-    function favQuotes() {
-        for (let key in currentQuote) {
-            if (currentQuote.hasOwnProperty(key)) {
-              console.log(key + " -> " + currentQuote[key]);
-            }
-          };
-        favQuotesContainer.innerHTML += `
-        <h2>${currentQuote.quote}</h2>
-        <h4>${currentQuote.author}</h4>
-        `
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+}
 
-
-
-
-        // currentQuote.forEach(el => {
-        //     favQuotesContainer.innerHTML = '';
-        //    EventTarget.[el].author = author;
-        //    [el].quote = quote;
-            // favQuotesContainer.innerHTML += `
-            // <h2>${quote}</h2>
-            // <h4>${author}</h4>
-            // `
-        // })
-    }
-
-    favQuotes();
-    //////////////// END ///////////////
-})
-// [0].author
-
- ////////////////////OLD CODE //////////////////////
+function likeQuote(event) {
+    let favQuotesContainer = document.querySelector(".fav-quotes-container")
+    favQuotesContainer.style.display = 'block';
+    let favQuote = document.querySelectorAll('em')[0].innerHTML;
+    let favAuthor = document.querySelectorAll('em')[1].innerHTML;
+    favQuotesContainer.innerHTML += `<p>${favQuote} </br>- ${favAuthor}</p></hr>`
+}
